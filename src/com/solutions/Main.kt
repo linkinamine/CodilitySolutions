c047748af8056dc4825f80b5314b9da7b95c2a21
Gulcihan Buyukdemircioglu <gulcihan.buyukdemircioglu@gmail.com>
2019-07-31 22:54:43 +0200

    Add solution for CyclicRotation problem

--- src/com/solutions/Main.kt
+++ src/com/solutions/Main.kt
@@ -7,5 +7,6 @@ object Main {
         // write your code here
         println("""BinaryGap -> ${BinaryGap.solution(9)}""")
         println("""OddOccurrencesInArray -> ${OddOccurrencesInArray.solution(intArrayOf(9, 3, 9, 3, 9, 7, 9))}""")
+        println("""CyclicRotation -> ${CyclicRotation.solution(intArrayOf(3, 8, 9, 7, 6), 3)}""")
     }
 }
